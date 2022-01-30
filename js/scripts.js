6258abf156e232b01854b7b4b50f91ecca7d839c
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-01-29 19:31:30 -1000

    add js show quiz, user submit name functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,23 @@ $(document).ready(function () {
   } else {
     $("#under-18").show();
   } });
+  
+  // show quiz on click
+  $(".clickable").click(() => {
+    $("#quiz-showing").slideDown();
+    $("#name1").fadeOut();
+  });
+
+  // when user submits name
+  $("#formOne").submit(function (event) {
+    const nameInput = $("input#name").val();
+    const answer5 = $("input:radio[name=answer5]:checked").val();
+
+    event.preventDefault();
+
+    $("#name1").fadeIn();
+    $("#formOne").slideUp();
+    $(".name").text(nameInput);
+    $(".name").append(" the great");
+    $(".name").show();
+  });
