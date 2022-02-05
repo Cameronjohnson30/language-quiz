df235921521de761074bd4db016093fcf835b4fd
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-02-05 15:39:32 -0800

    changed language-quiz.html to index.html, fixed spacing and inditation, add headings to readme

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
-Authored by Cameron Johnson,
+#Authored by Cameron Johnson,
 
-Language quiz
+##Language quiz
 
-Technologies Used
+###Technologies Used
 
-*HMTL
-*CSS
-*JavaScript
-*JQuery
-*Markdown
+**HMTL**
+**CSS**
+**JavaScript**
+**JQuery**
+**Markdown**
 
-Description: This website displays a quiz that results in different developer languages. It was constructed with the guidance of Epicodus.
+####Description: This website displays a quiz that results in different developer languages. It was constructed with the guidance of Epicodus.
 
-Setup/Installation Requirements
+#####Setup/Installation Requirements
 
 Clone or download the repository to your desktop.
-Open language-quiz.HTML file
+Open index.HTML file
 to alter the code, open the documents in an IDE or text document
 Any changes should incorporate properly formatted commits with regular history.
 if installing on a server, portfolio and related links must be updated to index.html
-can be scene at https://github.com/Cameronjohnson30/language-quiz.git
+can be scene at [title](https://github.com/Cameronjohnson30/language-quiz.git)
 Known Bugs
 
-No known bugs.
+######No known bugs.
 
-License This code utilizes a GPL License. If you have any issues/find bugs. Feel free to report them to me at here or contribute a fix to the code. Thanks! Copyright (c) 2022 Cameron Johnson
+#######License This code utilizes a GPL License. If you have any issues/find bugs. Feel free to report them to me at here or contribute a fix to the code. Thanks! Copyright (c) 2022 Cameron Johnson
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
-$(document).ready(function () {
+$(document).ready(function() {
   const over18 = confirm("Are you over 18? Click OK for yes or Cancel for no.");
 
-  // Turinary operator
-  // over18 ? $("#over-18").show() : $("#under-18").show();
-
-  // check if user is of age
   if (over18) {
     $("#over-18").show();
   } else {
     $("#under-18").show();
   }
 
-  // show quiz on click
   $(".clickable").click(() => {
     $("#quiz-showing").slideDown();
     $("#name1").fadeOut();
   });
 
-  // when user submits name
-  $("#formOne").submit(function (event) {
+  $("#formOne").submit(function(event) {
     const nameInput = $("input#name").val();
     const answer5 = $("input:radio[name=answer5]:checked").val();
 
@@ -31,8 +25,7 @@ $(document).ready(function () {
     $(".name").show();
   });
 
-  // when user submits quiz
-  $("#quiz").submit(function (event) {
+  $("#quiz").submit(function(event) {
     const q1 = parseInt($("#question1").val());
     const q2 = parseInt($("#question2").val());
     const q3 = parseInt($("#question3").val());
@@ -44,39 +37,33 @@ $(document).ready(function () {
 
     $("#result").html(
       `<h1>congrats you should learn ${whichLanguage(finalScore)}</h1>`
-    );
-  });
+  );
+  }); 
 
-  // reset form back to load state
-  $("#clear").click(function () {
+  $("#clear").click(function() {
     $(".form-control").val(0);
-    $("#result").html("");
+    $("#result").html("");  
   });
 });
 
-/*
- * Parses finalScore and returns a message.
- * @finalScore {number} calculated user score
- * @return {string} the correct message.
-*/
 function whichLanguage(finalScore) {
   let message = "";
   if (finalScore <= 5) {
-    // console.log("finalScore is greaterthan or equal to 5");
+
     message = "c#";
   } else if (finalScore <= 10) {
-    // console.log("finalScore is greaterthan or equal to 10");
+
     message = "ruby";
   } else if (finalScore <= 15) {
-    // console.log("finalScore is greaterthan or equal to 15");
+
     message = "python";
   } else if (finalScore <= 20) {
-    // console.log("finalScore is greaterthan or equal to 20");
+
     message = "java";
   } else {
-    // console.log("we have reached the else");
+  
     message = "nothing";
   }
-  // console.log(message);
+
   return message;
 }
